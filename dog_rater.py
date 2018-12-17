6e9ede2d755e10f7c6e140ab2290229eae551ace
Aaron <afink.2020@saintannsny.org>
2018-12-16 21:30:34 -0500

    Update dog_rater.py

--- dog_rater.py
+++ dog_rater.py
@@ -1,20 +1,20 @@
-# 1. Write a method called basic_rating that takes in a dog's name and returns a string that says "_____ is a cute dog. ___/10"
+# 1. Write a function called basic_rating that takes in a dog's name and returns a string that says "_____ is a cute dog. ___/10"
 #    For example, basic_rating("Spot") could return "Spot is a cute dog. 10/10"
 
 
 
 
-# 2. Write a method called special_rating that takes in a dog's name, returns a better rating if the dog's name is one of your favorite names.
+# 2. Write a function called special_rating that takes in a dog's name, returns a better rating if the dog's name is one of your favorite names.
 
 
 
 
 
-# 3. Write a method called type_rating that takes in a dog's breed, returns a better rating if the dog is one of your favorite types.
+# 3. Write a function called type_rating that takes in a dog's breed, returns a better rating if the dog is one of your favorite types.
 
 
 
 
-# 4. Write a method called ultimate_rating that takes in a dog's name and the dog's breed and returns a rating based on those two factors.
-#    BONUS 1: If the user doesn't specify the dog's breed, the method should default to assuming the dog's breed is "unknown".
+# 4. Write a function called ultimate_rating that takes in a dog's name and the dog's breed and returns a rating based on those two factors.
+#    BONUS 1: If the user doesn't specify the dog's breed, the function should default to assuming the dog's breed is "unknown".
 #    BONUS 2: If the dog's name is Apollo the Corgi, give the dog even more bonus points for being FAMOUS.
